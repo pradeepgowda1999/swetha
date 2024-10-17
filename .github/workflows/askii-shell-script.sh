93dee449bdc349f788df70d0656038cd10873833
pradeepgowda1999 <158177273+pradeepgowda1999@users.noreply.github.com>
2024-10-17 08:45:41 +0000

    edited the shell script file

--- .github/workflows/askii-shell-script.sh
+++ .github/workflows/askii-shell-script.sh
@@ -4,3 +4,9 @@ sudo apt-get install cowsay -y
 cowsay -f dragon "Run for cover, I am a Dragon ....RAWR" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
+echo "Good Morning"
+#good morning
+
+
+
+
